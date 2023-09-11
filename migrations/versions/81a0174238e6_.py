217ea87a458e21f50076f7c7fabbef12df29b43e
Ray Yu <yxr686@gmail.com>
2023-09-11 00:19:31 -0400

    update

--- migrations/versions/81a0174238e6_.py
+++ migrations/versions/81a0174238e6_.py
@@ -7,7 +7,9 @@ Create Date: 2023-09-10 23:00:12.693566
 """
 from alembic import op
 import sqlalchemy as sa
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '81a0174238e6'
