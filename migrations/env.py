a9baea3901ac6353f15423a05a4fb80ce882ff91
Ray Yu <yxr686@gmail.com>
2023-09-11 21:37:06 -0400

    update

--- migrations/env.py
+++ migrations/env.py
@@ -87,8 +87,9 @@ def run_migrations_online():
             connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
 
         with context.begin_transaction():
+            if environment == "production":
+                context.execute(f"SET search_path TO {SCHEMA}")
             context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
